476a1ff642112f5ef44858b10a3558e41ff082d8
Joshua Luppes <cooljoebob64@gmail.com>
2020-12-07 14:36:10 -0600

    Formatting updates

--- js/OOPDemo.js
+++ js/OOPDemo.js
@@ -1,39 +1,26 @@
-// console.log("JS Loaded!");
-
 class Person {
-
     constructor (first, last, weight) {
         this.first = first;
         this.last = last;
         this.weight = weight;
     }
-
     bio(){
         return (this.first + " " + this.last + ", weight: " + this.weight);
     }
 }
 
-
 class Tester extends Person {
-
     constructor(first,last,weight,bugs){
         super(first,last,weight);
         this.bugs = bugs;
     }
-
     bugCount(){
         return (this.bugs + " bugs found");
     }
-
     bio(){
         return (super.bio() + ", " + this.bugs + " bugs found");
     }
-
 }
-
-
-
-
 
 let monty = new Person("Monty", "Bananas", 300);
 console.log(monty.bio());
